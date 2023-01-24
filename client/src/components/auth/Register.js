38bc6b753b545f222cd1f7c67bd3e2dae6e94af8
Dylan Comeau <dcomeau@perscholas.org>
2023-01-24 10:36:11 -0500

    Alert Component and Action call

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import {Link} from 'react-router-dom'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-export const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -14,15 +17,15 @@ export const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        if(password !== password2){
-            console.log('Passwords do not match')
-        } else {
-            console.log('Success')
-        }
+    if (password !== password2) {
+      setAlert('Passwords do not match', 'danger');
+    } else {
+      console.log('Success');
     }
+  };
 
   return (
     <Fragment>
@@ -31,7 +34,7 @@ export const Register = () => {
         <p className='lead'>
           <i className='fas fa-user'></i> Create Your Account
         </p>
-        <form onSubmit={e => onSubmit(e)} className='form'>
+        <form onSubmit={(e) => onSubmit(e)} className='form'>
           <div className='form-group'>
             <input
               type='text'
@@ -87,3 +90,9 @@ export const Register = () => {
     </Fragment>
   );
 };
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
