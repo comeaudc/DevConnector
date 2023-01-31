82a1b7711696f33975825f56e51f210ceec153ef
Dylan Comeau <dcomeau@perscholas.org>
2023-01-31 13:54:36 -0500

    Post Item Component

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import ProfileItem from './ProfileItem'
+import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
@@ -10,19 +10,26 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
     getProfiles();
   }, [getProfiles]);
 
-  return <Fragment>
-    { profiles === null || loading ? <Spinner /> : <Fragment>
-        <h1 className='large text-primary'>Developers</h1>
-        <p className='lead'>
-            <i className='fab fa-connectdevelop'></i> Browse and connect with developers
-        </p>
-        <div className='profiles'>
-            {profiles.length > 0 ? (
-                profiles.map(profile => (<ProfileItem key={profile._id} profile={profile} />))
-            ) : <h4>No profiles found...</h4>}
-        </div>
-        </Fragment>}
-  </Fragment>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Developers</h1>
+      <p className='lead'>
+        <i className='fab fa-connectdevelop'></i> Browse and connect with
+        developers
+      </p>
+      <div className='profiles'>
+        {profiles.length > 0 ? (
+          profiles.map((profile) => (
+            <ProfileItem key={profile._id} profile={profile} />
+          ))
+        ) : (
+          <h4>No profiles found...</h4>
+        )}
+      </div>
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
